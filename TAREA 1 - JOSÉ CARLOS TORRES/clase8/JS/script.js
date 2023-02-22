03c2d784cba1dc33b8a799168be5a343cbe6f393
carlosmorenosvq <torresmorenojosecarlos@gmail.com>
2023-02-22 12:11:20 +0100

    actualizacion

--- TAREA 1 - JOSÉ CARLOS TORRES/clase8/JS/script.js
+++ TAREA 1 - JOSÉ CARLOS TORRES/clase8/JS/script.js
@@ -24,6 +24,20 @@ function procesarCargaJson() {
         }
 }
 
+function procesarCargaJson() {
+    let div = document.getElementById("resp");
+    if (conexion.readyState == 4 || conexion.readyState == 200) {
+        let datos = JSON.parse(conexion.responseText);
+        console.log(datos);
+        datos.results.forEach(dato => {
+            let p = document.createElement("p");
+            let texto = document.createTextNode(dato.name.first);
+            p.appendChild(texto);
+            div.appendChild(p);
+        });
+        }
+}
+
 let promesa = new Promise ();
 promesa.then();
 promesa.catch();
