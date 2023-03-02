4bb7f1224f8cdb67bc1e18b47ad90a52284a5835
carlosmorenosvq <torresmorenojosecarlos@gmail.com>
2023-03-02 11:05:18 +0100

    react

--- TAREA 1 - JOSÉ CARLOS TORRES/clase11/funciones.js
+++ TAREA 1 - JOSÉ CARLOS TORRES/clase11/funciones.js
@@ -1,9 +1,30 @@
-let oracion = "2..3";
+// let oracion = "2..3";
 
-let patron = /^[0-9]+[\?\][0-9]+$/
+// let patron = /^[0-9]+[\?\][0-9]+$/
 
-if(patron.test(oracion)){
+// if(patron.test(oracion)){
+//     console.log("Válido");
+// }else{
+//     console.log("No válido");
+// }
+
+let email = "hola@gmail.com";
+
+let patron2 = /[A-Za-z]\@\(gmail | hotmail | yahoo)\.\(com | es)/;
+
+if(patron2.test(email)){
     console.log("Válido");
 }else{
     console.log("No válido");
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ TAREA 1 - JOSÉ CARLOS TORRES/clase12/script.js
@@ -0,0 +1,46 @@
+
+
+// function manejaEventos(){
+//     let boton = document.getElementById("boton");
+//     boton.addEventListener("click", presionaBoton);
+// }
+
+// function presionaBoton(event) {
+//     alert("hola");
+// }
+
+// window.onload = manejaEventos;
+
+// let email = document.getElementById("email");
+
+// function manejaEventos(){
+//     email.addEventListener("blur", checkEmail);
+// }
+
+// function checkEmail(event) {
+//     let email = document.getElementById("email");
+
+//     let patron = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+//     if (patron.test(email.value)){
+//         alert("Mu bien");
+//     }else{
+//         alert("Mu mal");
+//     }
+// }
+
+// window.onload = manejaEventos;
+
+const fs = require("fs");
+
+
+
+
+
+
+
+
+
+
+
+
+
